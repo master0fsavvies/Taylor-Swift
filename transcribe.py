8d45f61b0d88fde11402e387996522ff2175fcd7
John Nix <136145027+master0fsavvies@users.noreply.github.com>
2025-03-17 20:15:42 -0700

    Update transcribe.py

--- transcribe.py
+++ transcribe.py
@@ -1,17 +1,29 @@
 import wave
 import json
+import os
 from vosk import Model, KaldiRecognizer
+import subprocess
 
-# File paths
+
 model_path = "model"
+mp3_path = "audio/YBWM.mp3"
 wav_path = "audio/YBWM.wav"
+
+subprocess.run([
+    "ffmpeg", "-i", mp3_path, 
+    "-ac", "1",              # Mono audio
+    "-ar", "16000",          # 16 kHz sample rate
+    "-sample_fmt", "s16",    # 16-bit audio
+    wav_path
+])
+song_path = wav_path
 
 # Load Vosk model
 model = Model(model_path)
 rec = KaldiRecognizer(model, 16000)  # Vosk expects 16kHz audio
 
 # Open WAV file
-with wave.open(wav_path, "rb") as wf:
+with wave.open(song_path, "rb") as wf:
     if wf.getnchannels() != 1 or wf.getsampwidth() != 2 or wf.getframerate() != 16000:
         raise ValueError("Audio must be mono, 16-bit, and 16kHz")
 
